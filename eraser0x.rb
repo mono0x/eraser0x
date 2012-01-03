2742c6a9853616a01655b67e98cd705d97f0339d
mono <mono@monoweb.info>
2012-01-03 15:52:06 +0900

    Refactored

--- eraser0x.rb
+++ eraser0x.rb
@@ -6,11 +6,114 @@ require 'twitter'
 require 'userstream'
 
 module Eraser
-  class Config
-    def self.load(path)
-      c = new()
-      c.instance_eval open(path).read
-      c
+  class MessageManager
+    def initialize(path)
+      @path = path
+      load
+    end
+
+    def load
+      @messages = JSON.parse(open(@path, 'r:utf-8').read)['messages']
+    end
+
+    def add(m)
+      unless @messages.include?(m)
+        @messages.push m
+        json = { 'messages' => @messages }.to_json
+        open(@path, 'w') do |f|
+          f << json
+        end
+      end
+    end
+
+    def get
+      @messages[rand @messages.size]
+    end
+  end
+
+  class Application
+    def initialize(config)
+      @config = config
+      @messages = MessageManager.new('messages.json')
+      reload
+    end
+
+    def reload
+      instance_eval open(@config).read
+    end
+
+    def run
+      twitter = ::Twitter::Client.new(
+        :consumer_key       => @oauth[:consumer_key],
+        :consumer_secret    => @oauth[:consumer_secret],
+        :oauth_token        => @oauth[:oauth_token],
+        :oauth_token_secret => @oauth[:oauth_token_secret])
+
+      consumer = ::OAuth::Consumer.new(
+        @oauth[:consumer_key],
+        @oauth[:consumer_secret],
+        :site => 'https://userstream.twitter.com/')
+
+      access_token = ::OAuth::AccessToken.new(
+        consumer,
+        @oauth[:oauth_token],
+        @oauth[:oauth_token_secret])
+
+      userstream = ::Userstream.new(consumer, access_token)
+      userstream.user do |status|
+        STDERR.puts status.inspect
+        case
+        when status.event == 'follow'
+          if status.source.screen_name != @account
+            unless twitter.friendship?(@account, status.source.screen_name)
+              twitter.follow status.source.id
+            end
+          end
+        when status.event == 'favorite'
+          next if status.source.screen_name == @account
+          next unless twitter.friendship?(status.source.screen_name, @account)
+          timeline = twitter.user_timeline(status.source.screen_name).select {|s|
+            !s.retweeted_status && !s.in_reply_to_status_id && !s.favorited
+          }
+          next if timeline.empty?
+          target = timeline[rand timeline.size]
+          begin
+            twitter.favorite_create target.id
+            twitter.retweet target.id
+          rescue
+          end
+        when status.direct_message
+          if status.direct_message.sender_screen_name == @author
+            twitter.update status.direct_message.text
+          end
+        when status.text
+          next if status.retweeted_status
+          if status.text =~ /\A@#{@account}\s+(.+)\Z/m
+            m = $1.gsub(/[@#]/, '_')
+            unless twitter.friendship?(status.user.screen_name, @account)
+              twitter.unfollow status.user.id
+              next
+            end
+            @messages.add m
+            twitter.update(
+              "@#{status.user.screen_name} #{@messages.get}",
+              :in_reply_to_status_id => status.id)
+            next
+          end
+          next unless status.text =~ @pattern
+          unless twitter.friendship?(status.user.screen_name, @account)
+            twitter.unfollow status.user.id
+            next
+          end
+          twitter.favorite_create status.id
+          twitter.retweet status.id
+          if rand < 0.05
+            twitter.update(
+              "@#{status.user.screen_name} #{@messages.get}",
+              :in_reply_to_status_id => status.id)
+          end
+        end
+      end
     end
 
     attr_reader :account
@@ -20,91 +123,12 @@ module Eraser
   end
 end
 
-messages = JSON.parse(open('messages.json', 'r:utf-8').read)['messages']
-
-config = Eraser::Config.load('eraser.conf')
-
-Twitter.configure do |c|
-  c.consumer_key       = config.oauth[:consumer_key]
-  c.consumer_secret    = config.oauth[:consumer_secret]
-  c.oauth_token        = config.oauth[:oauth_token]
-  c.oauth_token_secret = config.oauth[:oauth_token_secret]
-end
-
-consumer = OAuth::Consumer.new(
-  config.oauth[:consumer_key],
-  config.oauth[:consumer_secret],
-  :site => 'https://userstream.twitter.com/')
-
-access_token = OAuth::AccessToken.new(
-  consumer,
-  config.oauth[:oauth_token],
-  config.oauth[:oauth_token_secret])
+app = Eraser::Application.new('eraser.conf')
 
 Signal.trap :HUP do
   STDERR.puts 'SIGHUP'
-  config = Eraser::Config.load('eraser.conf')
+  app.reload
 end
 
-userstream = Userstream.new(consumer, access_token)
-userstream.user do |status|
-  STDERR.puts status.inspect
-  case
-  when status.event == 'follow'
-    if status.source.screen_name != config.account
-      unless Twitter.friendship?(config.account, status.source.screen_name)
-        Twitter.follow status.source.id
-      end
-    end
-  when status.event == 'favorite'
-    next if status.source.screen_name == config.account
-    next unless Twitter.friendship?(status.source.screen_name, config.account)
-    timeline = Twitter.user_timeline(status.source.screen_name).select {|s|
-      !s.retweeted_status && !s.in_reply_to_status_id && !s.favorited
-    }
-    next if timeline.empty?
-    target = timeline[rand timeline.size]
-    begin
-      Twitter.favorite_create target.id
-      Twitter.retweet target.id
-    rescue
-    end
-  when status.direct_message
-    if status.direct_message.sender_screen_name == config.author
-      Twitter.update status.direct_message.text
-    end
-  when status.text
-    next if status.retweeted_status
-    if status.text =~ /\A@#{config.account}\s+(.+)\Z/m
-      m = $1.gsub(/[@#]/, '_')
-      unless Twitter.friendship?(status.user.screen_name, config.account)
-        Twitter.unfollow status.user.id
-        next
-      end
-      unless messages.include?(m)
-        messages.push m
-        json = { 'messages' => messages }.to_json
-        open('messages.json', 'w') do |f|
-          f << json
-        end
-      end
-      Twitter.update(
-        "@#{status.user.screen_name} #{messages[rand messages.size]}",
-        :in_reply_to_status_id => status.id)
-      next
-    end
-    next unless status.text =~ config.pattern
-    unless Twitter.friendship?(status.user.screen_name, config.account)
-      Twitter.unfollow status.user.id
-      next
-    end
-    Twitter.favorite_create status.id
-    Twitter.retweet status.id
-    if rand < 0.05
-      Twitter.update(
-        "@#{status.user.screen_name} #{messages[rand messages.size]}",
-        :in_reply_to_status_id => status.id)
-    end
-  end
-end
+app.run
 
